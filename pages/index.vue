1169c612fdafe6953a3fafefad0901e26e8f0e87
Tnaka-Kizuki <tnk_kzk5921@outlook.jp>
2020-11-08 20:19:45 +0900

    検索機能追加

--- pages/index.vue
+++ pages/index.vue
@@ -1,34 +1,58 @@
 <template>
   <div class="container">
     <div>
-      <Logo />
-      <h1 class="title">
-        book-search
-      </h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <input type="text" v-model="isbn">
+      <button class="button" v-on:click="search">Search</button>
+      <h2 class="title">
+        {{ title }}
+        {{ author }}
+        {{ publisher }}
+        {{ pubdate }}
+      </h2>
+      <img v-bind:src="cover">
     </div>
   </div>
 </template>
-
 <script>
-export default {}
+const axios = require("axios");
+export default {
+  data:()=>({
+    title:"",
+    author:"",
+    publisher:"",
+    pubdate:"",
+    cover:"",
+    isbn:"",
+  }),
+  methods:{
+    search(){
+      const url = "https://api.openbd.jp/v1/get?isbn=" + this.isbn;
+      axios.get(url)
+      .then((res) => {
+        console.log(res.data[0].summary)
+        this.title = res.data[0].summary.title;
+        this.author = res.data[0].summary.author;
+        this.publisher = res.data[0].summary.publisher;
+        this.pubdate = res.data[0].summary.pubdate;
+        this.cover = res.data[0].summary.cover;
+      })
+    }
+  }
+}
+// const isbn = "978-4-7741-8967-3";
+// const url = "https://api.openbd.jp/v1/get?isbn=" + isbn;
+// export default {
+//   asyncData({ params, error }) {
+//     return axios
+//       .get(url)
+//       .then((res) => {
+//         return { title: res.data[0].summary.title };
+//       })
+//       .catch((e) => {
+//         error({ title: e.response.status, message: "ERROR!!" });
+//       });
+//   },
+// };
 </script>
 
 <style>
@@ -54,7 +78,7 @@ export default {}
     sans-serif;
   display: block;
   font-weight: 300;
-  font-size: 100px;
+  font-size: 10px;
   color: #35495e;
   letter-spacing: 1px;
 }
